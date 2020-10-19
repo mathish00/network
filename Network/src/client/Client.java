397a4d8462b785c660331c4434ddce5752a1d965
mhoff <mhoff@DESKTOP-3VK3FV6.fritz.box>
2020-10-20 01:11:30 +0200

    Server: added an integer array for multiple ports to listen to Client: separated the connection to multiple methods in order to allow longer client-server-exchange

--- Network/src/client/Client.java
+++ Network/src/client/Client.java
@@ -1,36 +1,75 @@
 package client;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
+
+import org.json.*;
 
 public class Client {
 
 	public InetSocketAddress address;
+	Socket server;
 	
 	public static void main(String[] args) {
 		Client client = new Client("localhost", 8000);
+		try {
+			client.sendMessage("gude!", "Mathis");
+		} catch (InterruptedException e) {
+			System.out.println("[Client] An error occurred.");
+		}
+		
 
+	}
+	
+	public void connectToServer() {
+		server = new Socket();
+		try {
+			server.connect(address, 10000);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void disconnectFromServer() {
+		try {
+			server.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public Client(String hostname, int port) {
 		address = new InetSocketAddress(hostname, port);
 	}
 	
-	public void sendMessage(String msg) {
+	public void sendMessage(String msg, String recipient) throws InterruptedException {
 		try {
-			Socket socket = new Socket();
-			socket.connect(address, 10000);
-			PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
 			
-			pw.write(msg);
+			JSONObject jo = new JSONObject();
+			jo.putOpt("recipient", recipient);
+			jo.putOpt("message", msg);
+			
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(server.getOutputStream())));
+
+			pw.write(jo.toString());
 			pw.flush();
 			
+			Scanner s =  new Scanner(new BufferedReader(new InputStreamReader(server.getInputStream())));
+			//this.wait(2000);
+			//while(s.hasNext())
+			//	System.out.println("[Client] Received answer by the server: " + s.next());
+			
+			s.close();
 			pw.close();
-			socket.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- Network/src/server/Server.java
+++ Network/src/server/Server.java
@@ -1,23 +1,29 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
+import org.json.*;
+
 public class Server {
 
-	public int port;
+	public int[] ports;
 	
 	public static void main(String[] args) {
-		new Server(8000).startListening();;
+		Server s = new Server();
+		s.startListening();
 
 	}
 	
-	public Server(int port) {
-		this.port = port;
+	public Server() {
+		createPorts();
 	}
 	
 	public void startListening() {
@@ -29,13 +35,29 @@ public class Server {
 				System.out.println("[Server] Server is starting");
 				while(true) {
 					try {
-						ServerSocket socket = new ServerSocket(port);
-						System.out.println("[Server] Waiting for connections");
+						//get ready for connections
+						ServerSocket socket = new ServerSocket(ports[0]);
+						System.out.println("[Server port 0] Waiting for connections");
 						Socket client = socket.accept();
-						System.out.println("[Server] Connection established");
+						System.out.println("[Server port 0] Connection established");
+						
+						//receive a message
 						Scanner s = new Scanner(new BufferedReader(new InputStreamReader(client.getInputStream())));
-						while(s.hasNext())
-							System.out.println("[Server] Received message: " + s.next());
+						
+						JSONObject reception = new JSONObject(s.next());
+						//TODO: on which port can the server find the specified recipient?
+						String recipient = reception.getString("recipient");
+						
+						System.out.println("[Server port 0] " + reception.get("message"));
+						
+						//send an answer to the client 
+						PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())));
+						pw.write("This is an auto-generated answer by the server.");
+						pw.flush();
+						
+						
+						//close 
+						pw.close();
 						s.close();
 						client.close();
 						socket.close();
@@ -49,6 +71,17 @@ public class Server {
 			}
 			
 		}).run();
+	}
+	
+	/**
+	 * Create 10 ports for possible clients
+	 * @return
+	 */
+	private void createPorts() {
+		int[] ports = new int[10];
+		for(int i = 0; i < 10; i++)
+			ports[i] = 8000 + i;
+		this.ports = ports;
 	}
 
 }
